82fe3c1523f68bdd7d9c9b15b1890d214c01c9fb
julithedrummer24 <julianandresyamartecastellanos@gmail.com>
2025-10-28 12:15:45 -0500

    importes

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
